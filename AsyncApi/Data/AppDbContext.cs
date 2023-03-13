003f32ee45cba30af21bc5fbda0f2e988c0b49d9
Saba Gotsiridze <s.gotsiridze@altasoft.ge>
2023-03-13 15:06:59 +0400

    ref

---
+++ AsyncApi/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AsyncApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsyncApi.Data;
+
+public class AppDbContext : DbContext
+{
+	public DbSet<ListingRequest> ListingRequests => Set<ListingRequest>();
+
+	public AppDbContext(DbContextOptions options) 
+		: base(options)
+	{ }
+}
